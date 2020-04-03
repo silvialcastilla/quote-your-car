0c77b858e8aef270c159f6b6a18dcda41a4f6829
Tropical23 <silope0302@gmail.com>
2020-04-03 13:33:21 +0200

    proyecto maquetado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
-import Header from './components/Header';
-import Formulario from './components/Formulario';
-import Resumen from './components/Resumen';
-import Resultado from './components/Resultado';
-import styled from '@emotion/styled';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import Resumen from "./components/Resumen";
+import Resultado from "./components/Resultado";
+import styled from "@emotion/styled";
 
 const Contenedor = styled.div`
   max-width: 600px;
@@ -11,38 +11,31 @@ const Contenedor = styled.div`
 `;
 
 const ContenedorFormulario = styled.div`
-  background-color:  #FFF;
+  background-color: #fff;
   padding: 3rem;
 `;
 
 function App() {
-const [resumen, guardarResumen]  = useState({
-  cotizacion: 0,
-  datos: {
-    marca: '',
-    year: '',
-    plan: ''
-  }
-});
+  const [resumen, guardarResumen] = useState({
+    cotizacion: 0,
+    datos: {
+      marca: "",
+      year: "",
+      plan: ""
+    }
+  });
 
-//extraer datos
+  //extraer datos
 
-const {cotizacion, datos} = resumen
+  const { cotizacion, datos } = resumen;
 
   return (
     <Contenedor>
-      <Header
-        titulo='Cotizador de seguros'
-      />
+      <Header titulo="Cotizador de seguros" />
       <ContenedorFormulario>
-        <Formulario
-          guardarResumen={guardarResumen}
-        />
-        <Resumen
-        datos={datos} />
-        <Resultado
-        cotizacion={cotizacion} 
-        />
+        <Formulario guardarResumen={guardarResumen} />
+        <Resumen datos={datos} />
+        <Resultado cotizacion={cotizacion} />
       </ContenedorFormulario>
     </Contenedor>
   );
--- src/components/Resumen.js
+++ src/components/Resumen.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import { primerMayuscula } from '../helper';
+import React from "react";
+import styled from "@emotion/styled";
+import { primerMayuscula } from "../helper";
 
 const ContenedorResumen = styled.div`
-    padding: 1rem;
-    text-align: center;
-    background-color: #00838F;
-    color: #FFF;
-    margin-top: 1rem;
+  padding: 1rem;
+  text-align: center;
+  background-color: #00838f;
+  color: #fff;
+  margin-top: 1rem;
 `;
 
+const Resumen = ({ datos }) => {
+  //extraer datos
+  const { marca, year, plan } = datos;
 
-const Resumen = ({datos}) => {
-    //extraer datos
-     const {marca, year, plan} = datos;
+  if (marca === "" || year === "" || plan === "") {
+    return null;
+  }
 
-     if (marca === '' || year === '' || plan ==='') {
-        return null
-     }
-    
-    return ( 
-        <ContenedorResumen>
-        <h2>Resumen de cotización</h2>
-        <ul>
-            <li>Marca: {primerMayuscula (marca)} </li>
-            <li>Plan: {primerMayuscula (plan)} </li>
-            <li>Año del auto: {year}</li>
-        </ul>
-        </ContenedorResumen>
-     );
-}
- 
+  return (
+    <ContenedorResumen>
+      <h2>Resumen de cotización</h2>
+      <ul>
+        <li>Marca: {primerMayuscula(marca)} </li>
+        <li>Plan: {primerMayuscula(plan)} </li>
+        <li>Año del auto: {year}</li>
+      </ul>
+    </ContenedorResumen>
+  );
+};
+
 export default Resumen;
